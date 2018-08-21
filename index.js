252e8424fe7d805d908c4fae15b1ec63e2751193
wajih <wajih.ben-ali@daveo.fr>
2018-08-21 11:00:35 +0200

    simple response

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ server.post('',(req,res)=>{
     //         let dataToSend= movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some infon on 'The Godfather' instead.\n` : '';
     //         dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
    // const actionDF = req['queryResult']['action'];
-   const actionDF= "ed";
+   const actionDF= req.queryResult.action;
     if(actionDF === "input.welcome")
     {
         return res.json({
