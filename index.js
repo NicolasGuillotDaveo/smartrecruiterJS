5f57fd6ca371b39e73b70b844cd00bbea640058a
wajih <wajih.ben-ali@daveo.fr>
2018-08-21 10:33:27 +0200

    simple response

--- index.js
+++ index.js
@@ -21,22 +21,20 @@ server.post('',(req,res)=>{
     //         const movie = JSON.parse(completeResponse);
     //         let dataToSend= movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some infon on 'The Godfather' instead.\n` : '';
     //         dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
-    const actionDF = req && req.queryResult && req.queryResult.action;
+    const actionDF = req['queryResult']['action'];
     if(actionDF === "input.welcome")
     {
-        // return res.json({
-        //     fulfillmentText: `Bienvenue prénom, je m'appelle Léo, je suis le dernier né du Lab de Daveo.`,
-        //     source: 'webhook node js'
-        // });
-        return res.json(req);
+        return res.json({
+            fulfillmentText: `Bienvenue prénom, je m'appelle Léo, je suis le dernier né du Lab de Daveo.`,
+            source: 'webhook node js'
+        });
     }
     else
     {
-//         return res.json({
-//             fulfillmentText: "cc",
-//             source: 'webhook node js'
-//    });
-return res.json(JSON.parse(req));
+        return res.json({
+            fulfillmentText: actionDF,
+            source: 'webhook node js'
+   });
     }    
     //     });
      }
