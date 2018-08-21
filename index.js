720588239cf873cfebad3f4b8416a48963bd8323
wajih <wajih.ben-ali@daveo.fr>
2018-08-21 12:07:39 +0200

    simple response

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ server.post('',(req,res)=>{
     //         let dataToSend= movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some infon on 'The Godfather' instead.\n` : '';
     //         dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
    // const actionDF = req['queryResult']['action'];
-   let actionDF = req.body.queryResult && req.body.queryResult.action ? req.body.queryResult.action :'unknown';
-   let basicAnswer = res.body.queryResult && req.body.queryResult.fulfillmentText ? req.body.queryResult.fulfillmentText :'unknown';
+   let action = req.body.queryResult && req.body.queryResult.action ? req.body.queryResult.action :'unknown';
+   //let basicAnswer = res.body.queryResult && req.body.queryResult.fulfillmentText ? req.body.queryResult.fulfillmentText :'unknown';
     
    switch(actionDF){
     case "input.welcome":
@@ -41,7 +41,7 @@ server.post('',(req,res)=>{
      
      default: 
      return res.json({
-        fulfillmentText: basicAnswer,
+        fulfillmentText: 'basicanswer',
         source: 'webhook node js'
 });
    }
